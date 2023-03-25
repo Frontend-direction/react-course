f23c35f35c5ee336d7088b203736cfefd2e40b67
vovapankiw <vovapankiw@gmail.com>
2023-03-25 16:16:07 +0200

    added first approach

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,16 +1,38 @@
 import './index.css';
 import {useState} from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log(title);
+    setBooks(prev => [...prev, { title, id: Math.round(Math.random() * 999) }]);
+  }
+
+  const deleteBook = (id) => {
+    const updatedBooks = books.filter(book => {
+      return book.id !== id
+    });
+
+    setBooks(updatedBooks);
+  }
+
+  const editBook = (id, newTitle) => {
+    const updatedBooks = books.map(book => {
+      if(book.id === id) {
+        return { ...book, title: newTitle }
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
   }
 
   return (
-    <div>
+    <div className='app'>
+      <BookList books={books} onDelete={deleteBook} onEdit={editBook}/>
       <BookCreate onCreate={createBook} />
     </div>
   )
--- book-list/src/components/BookShow.js
+++ book-list/src/components/BookShow.js
@@ -1,5 +1,37 @@
-function BookShow() {
-  return <>asdsad</>
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({book, onDelete, onEdit}) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDelete = () => {
+    onDelete(book.id);
+  }
+  const handleEdit = () => {
+    setShowEdit(!showEdit);
+  }
+
+  const onSubmit = (id, title) => {
+    onEdit(id, title);
+    setShowEdit(false);
+  }
+
+  let content = <h3>{book.title}</h3>;
+  if(showEdit) {
+    content = <BookEdit  book={book} onSubmit={onSubmit}/>
+  }
+
+  return (
+    <div className="book-show">
+      <div>{content}</div>
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+      <div className='actions'>
+        <button className='edit' onClick={handleEdit}>Edit</button>
+        <button className="delete" onClick={handleDelete}>Delete</button>
+      </div>
+     
+    </div>
+  )
 }
 
 export default BookShow;
