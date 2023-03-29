3dc41c89025b8c427966b097aaebd32df6c83b26
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-03-29 12:48:15 +0300

    added props

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,10 +1,19 @@
 import Button from "./Button";
 import './index.css';
+import { GoBell} from 'react-icons/go'
+
+
 
 function App() {
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+  
   return (
     <div className="App">
-      <Button>OK</Button>
+    
+      <Button onClick={handleClick}><GoBell />Ok</Button>
     </div>
   );
 }
