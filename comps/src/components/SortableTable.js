750d0dabbf3b4814dd0d7b5eed7f436531d56fea
vovapankiw <vovapankiw@gmail.com>
2023-04-02 18:31:59 +0300

    added custom hook

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,31 +1,10 @@
-import { useState } from "react";
 import Table from "./Table";
 import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go';
+import useSort from "../hooks/use-sort";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if(sortBy && label !== sortBy) {
-      setSortOrder('asc');
-      setSortBy(label);
-      return;
-    }
-
-    if(sortOrder === null) {
-      setSortOrder('asc');
-      setSortBy(label);
-    } else if (sortOrder === 'asc') {
-      setSortOrder('desc');
-      setSortBy(label);
-    } else if (sortOrder === 'desc') {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  }
+  const { sort, sortBy, sortOrder, sortedData } = useSort(data, config);
 
   const updatedConfig = config.map(column => {
     if(!column.sortValue) return column;
@@ -33,29 +12,13 @@ function SortableTable(props) {
     return {
     ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
+        <th onClick={() => sort(column.label)}>
           {getIcons(column.label, sortBy, sortOrder)}
           {column.label}
         </th> 
       )
     }
   });
-
-  let sortedData = data;
-  if(sortOrder && sortBy) {
-    const { sortValue } = config.find(column => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-     const valueA = sortValue(a);
-     const valueB = sortValue(b);
-     const reverseOrder = sortOrder === 'asc' ? 1 : -1;
-
-     if(typeof valueA === 'string') {
-      return valueA.localeCompare(valueB);
-     } else {
-      return (valueA - valueB) * reverseOrder;
-     }
-    });
-  }
 
   return (
     <div>
---
+++ comps/src/pages/CounterPage.js
@@ -0,0 +1,18 @@
+import useCounter from '../hooks/use-counter';
+import Button from '../components/Button';
+
+function CounterPage({ initialCount }) {
+  const { count, increment } = useCounter(initialCount);
+
+  return (
+    <div>
+      Count is{count}
+      <Button onClick={increment}>
+        Increment
+      </Button>
+    </div>
+  )
+}
+
+
+export default CounterPage;
