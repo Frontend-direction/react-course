b38ca3ab24fe086b543e5e8a06e86f19ec682e55
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-04-05 17:52:14 +0300

    added context

---
+++ tick-tick-toe/src/context/game-context.js
@@ -0,0 +1,36 @@
+import { createContext, useState } from 'react';
+import TickTickToe from '../game/TickTickToe';
+
+const GameContext = createContext();
+const game = new TickTickToe();
+
+const GameProvider = ({ children }) => {
+  const [board, setBoard] = useState([]);
+
+  const move = (cell) => {
+    game.move(cell);
+    setBoard(game.gameBoard);
+  }
+
+  const startGame = (gameMode, firstPlayer) => {
+    game.start();
+    setBoard(game.gameBoard);
+  }
+
+  const share = {
+    startGame,
+    board,
+    paleyerMove: move,
+    score: game.score
+  }
+  
+  return (
+    <GameContext.Provider value={share}>
+      {children}
+    </GameContext.Provider>
+  )
+}
+
+export { GameProvider };
+
+export default GameContext;
