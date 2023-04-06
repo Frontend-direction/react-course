17bc0a5aaa60a7745108eb31e8cd1434c2b4108e
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-04-06 14:53:41 +0300

    added todo

--- tick-tick-toe/src/services/tick-tick-toe/ComputerPlayer.js
+++ tick-tick-toe/src/services/tick-tick-toe/ComputerPlayer.js
@@ -1,6 +1,7 @@
 import { Player } from "./Player";
 
 class ComputerPlayer extends Player {
+  // @TODO: Implement strategies depends on game mode (easy, difficult)
   getMoveIndex(board, index) {
     let randomIndex = Math.floor(Math.random() * board.length);
 
